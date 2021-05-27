bc594ca1ba5eb6549cf915788de931ebc936d732
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-05-27 14:36:07 +0200

    First commit

---
+++ src/js/menu.js
@@ -0,0 +1,22 @@
+const menuBtnRef = document.querySelector('.menu-button');
+const menuLinkRefs = document.querySelectorAll('[data-menu-link]');
+const mobileMenuRef = document.querySelector('[data-menu]');
+
+menuBtnRef.addEventListener('click', () => {
+  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+  menuBtnRef.classList.toggle('is-open');
+  menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+  mobileMenuRef.classList.toggle('is-open');
+  return;
+});
+
+menuLinkRefs.forEach(function (menuLinkRef) {
+  menuLinkRef.addEventListener('click', () => {
+    menuBtnRef.classList.remove('is-open');
+    menuBtnRef.setAttribute('aria-expanded', false);
+
+    mobileMenuRef.classList.remove('is-open');
+  });
+});
