49a2f44afe4b9c2bc0e7231c5b7e1e62686ce328
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-05-27 23:37:15 +0200

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,14 +4,28 @@
     openModalMenuBtn: document.querySelector('[data-modal-menu-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('div.backdrop'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.openModalMenuBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', openCloseModalMenu);
+  refs.openModalMenuBtn.addEventListener('click', openCloseModalMenu);
+  refs.closeModalBtn.addEventListener('click', openCloseModalMenu);
+  refs.backdrop.addEventListener('click', openCloseModalMenu);
 
-  function toggleModal() {
+  function openCloseModalMenu(event) {
     document.body.classList.toggle('modal-open');
     refs.modal.classList.toggle('is-hidden');
   }
+
+  function closeModalMenu(event) {
+    document.body.classList.remove('modal-open');
+    refs.modal.classList.add('is-hidden');
+  }
+
+  window.onkeydown = function (event) {
+    if (event.keyCode === 27) {
+      closeModalMenu(event);
+      return;
+    }
+  };
 })();
